06d9e11de3ee61b6533b879c47d75ee9a16f1fdf
Rajesh kumar Yadav <112757271+rajeshyadav9931@users.noreply.github.com>
2024-07-25 13:35:46 +0530

    Update PDFViewer.jsx

--- frontend/src/component/PDFViewer.jsx
+++ frontend/src/component/PDFViewer.jsx
@@ -3,21 +3,24 @@ import { Document, Page } from "react-pdf/dist/esm/entry.webpack"
 import PDFfile from "../assets/sample.pdf"
 
 function PDFViewer() {
-  const [numPages,setNumPages]=useState(null)
-  function onDocumentSuccess({numPages}){
-         setNumPages(numPages)
-  }
+  // const [numPages,setNumPages]=useState(null)
+  // function onDocumentSuccess({numPages}){
+  //        setNumPages(numPages)
+  // }
 
   return (
-    <div style={{display:"flex", justifyContent:"left"}}>
-        <div style={{margin:"5px", width:"800px", height:"850px", border:"3px solid black"}}>
-             <Document file={PDFfile} onLoadSuccess={onDocumentSuccess}>
-             {
-                  Array(numPages).fill().map((_,i) =>(<Page pageNumber={i+1}> </Page>))
-             }
-             </Document>
-        </div>
-    </div>
+    // <div style={{display:"flex", justifyContent:"left"}}>
+    //     <div style={{margin:"5px", width:"800px", height:"850px", border:"3px solid black"}}>
+    //          <Document file={PDFfile} onLoadSuccess={onDocumentSuccess}>
+    //          {
+    //               Array(numPages).fill().map((_,i) =>(<Page pageNumber={i+1}> </Page>))
+    //          }
+    //          </Document>
+    //     </div>
+    // </div>
+    <>
+      <iframe src={PDFfile} className="w-[50%] h-[100vh] border-none"></iframe>
+    </>
   )
 }
 
